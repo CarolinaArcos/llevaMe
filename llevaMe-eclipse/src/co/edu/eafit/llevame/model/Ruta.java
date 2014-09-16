9b2c406401d74704dc2f6bc4dd3df06884ad3101
CarolinaArcos <caroarcos09@hotmail.com>
2014-09-16 16:42:17 -0500

    Se implemento primera version del formato para crear una ruta y almacenarla en la base de datos

--- llevaMe-eclipse/src/co/edu/eafit/llevame/model/Ruta.java
+++ llevaMe-eclipse/src/co/edu/eafit/llevame/model/Ruta.java
@@ -2,16 +2,22 @@ package co.edu.eafit.llevame.model;
 
 import java.util.ArrayList;
 
+import co.edu.eafit.llevame.database.DatabaseHandler;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
 public class Ruta {
 	
 	//nombre de la ruta
 	private String name;
 	
 	//fecha en que ocurre la ruta (TODO: definir el tipo de dato)
-	private Object date;
+	private String date;
 	
 	//hora en que ocurre la ruta (TODO: verificar si en fecha se incluye la hora)
-	private Object hour;
+	private String hour;
 	
 	//capacidad maxima de la ruta
 	private int capacity;
@@ -19,8 +25,46 @@ public class Ruta {
 	//placa del vehiculo
 	private String placa;
 	
+	//descripcion de la ruta
+	private String descripcion;
+	
 	//ruta como tal a llevar a cabo
 	private ArrayList<Ubicacion> ruta;
 	
+	public Ruta(String name, String date, String hour, int capacity,
+			String placa, String descripcion) {
+		
+		this.name = name;
+		this.date = date;
+		this.hour = hour;
+		this.capacity = capacity;
+		this.placa = placa;
+		this.descripcion = descripcion;
+	}
+
+
+
+
+	public void insertarEnDB(Context context) {
+		
+		DatabaseHandler dbh = new DatabaseHandler(context);
+		SQLiteDatabase db = dbh.getReadableDatabase();
+		
+		ContentValues values = new ContentValues();
+		values.put(DatabaseHandler.KEY_NAME, this.name);
+		values.put(DatabaseHandler.KEY_DATE, this.date);
+		values.put(DatabaseHandler.KEY_HOUR, this.hour);
+		values.put(DatabaseHandler.KEY_CAPACITY, this.capacity);
+		values.put(DatabaseHandler.KEY_NUMBERPLATE, this.placa);
+		values.put(DatabaseHandler.KEY_DETAILS, this.descripcion);
+		
+		//TODO: put mapa
+		// TODO: put puntos mapa
+		
+		
+		db.insert(DatabaseHandler.TABLE_ROUTES, null, values);
+		
+		
+	}
 	
 }
---
+++ llevaMe-eclipse/src/co/edu/eafit/llevame/view/FormularioCrearRuta.java
@@ -0,0 +1,62 @@
+package co.edu.eafit.llevame.view;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import co.edu.eafit.llevame.R;
+import co.edu.eafit.llevame.model.Ruta;
+
+public class FormularioCrearRuta extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_formulario_crear_ruta);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.formulario_crear_ruta, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+	
+	public void enviarFormulario(View view) {
+		EditText hora = (EditText) findViewById(R.id.horaFormulario);
+		EditText fecha = (EditText) findViewById(R.id.fechaFormulario);
+		EditText cupo = (EditText) findViewById(R.id.cupoFormulario);
+		EditText placa = (EditText) findViewById(R.id.placaFormulario);
+		EditText descripcion = (EditText) findViewById(R.id.descripcionFormulario);
+		EditText nombre = (EditText) findViewById(R.id.nombreRutaFormulario);
+		
+		String dataHora = hora.getText().toString();
+		String dataFecha = fecha.getText().toString();
+		String dataCupo = cupo.getText().toString();
+		String dataPlaca = placa.getText().toString();
+		String dataDescripcion = descripcion.getText().toString();
+		String dataName = nombre.getText().toString();
+		
+		//TODO: obtener el nombre de usuario que inicio sesion
+		
+		//TODO: obtener mapa
+		
+		Ruta ruta = new Ruta(dataName, dataFecha, dataHora, Integer.parseInt(dataCupo), dataPlaca, dataDescripcion);
+		
+		ruta.insertarEnDB(this);
+	}
+}
